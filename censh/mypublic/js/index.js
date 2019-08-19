1902fc4d877bf779d4eda27d0ac39d2aef8535c1
z <1570623976@qq.com>
2019-08-19 23:56:57 +0800

    首页tab部分

---
+++ censh/mypublic/js/index.js
@@ -0,0 +1,21 @@
+$(function(){
+    $("[data-toggle=tab]").mouseenter(function(){
+        $(this).parent()
+        //li
+        .addClass("active").siblings().removeClass("active")
+        //ol
+        .parent()
+        
+        //兄弟
+        .next()//div
+        //.addClass("color")
+        //不管怎么变换，始终保持都是第一个孩子被选中，无法一对一的应对
+        .children(":first-child")
+        .addClass("color")
+        //.addClass("active").siblings().removeClass("active");
+        //.siblings().removeClass("color");
+    })
+    $("[data-toggle=tab-one]").mouseenter(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+    })
+})
