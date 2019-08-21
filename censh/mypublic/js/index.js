daf27fb41a13a10b3de1a7313148dc7a9f5a17a2
z <1570623976@qq.com>
2019-08-21 21:23:26 +0800

    首页tab完整实现

--- censh/mypublic/js/index.js
+++ censh/mypublic/js/index.js
@@ -1,22 +1,28 @@
 $(function(){
+    //hot的tab切换
     $("[data-toggle=tab]").mouseenter(function(){
         var i=parseInt($(this).attr("data-index"));
-        //console.log(i);
-        var ul=$(this).parent()
+        //给每一个li加点击效果
+        $(this).parent()
         //li
-        .addClass("active").siblings().removeClass("active")
-        //ol
-        .parent()
-        //兄弟
-        .next()//div
-        //不管怎么变换，始终保持都是第一个孩子被选中，无法一对一的应对
-        .children(["ul#u$"])
-        
-        //.addClass("u${i}")
-        //.addClass("active").siblings().removeClass("active");
-        //.siblings().removeClass("color");
+        .addClass("active").siblings().removeClass("active");
+        //给对应的下标设置是否显示
+        $(`#u${i}`).addClass("active").siblings().removeClass("active");
     })
+    //store的tab切换
+    $("[data-toggle=tab-two]").mouseenter(function(){
+        var i=parseInt($(this).attr("data-index"));
+        //给每一个a加点击效果
+        $(this).addClass("actived").siblings().removeClass("actived");
+        //给对应的下标设置是否显示
+        $(`#ul-${i}`).addClass("active").siblings().removeClass("active");
+    })
+    //rank的tab切换
     $("[data-toggle=tab-one]").mouseenter(function(){
+        var i=parseInt($(this).attr("data-index"));
+        //给自己添加效果
         $(this).addClass("active").siblings().removeClass("active");
+        //给对应的下标设置是否显示
+        $(`#listu${i}`).addClass("active").siblings().removeClass("active");
     })
 })
